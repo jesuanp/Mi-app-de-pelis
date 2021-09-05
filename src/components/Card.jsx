ca535cbcfc0fb676740a7c4202c82c78326dd2ac
jesuan <jespaterm@gmail.com>
2021-09-05 10:18:22 +0800

    Pequeños cambios

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useRef } from 'react'
-import { busqueda } from '../reduxs/actions'
+import React from 'react'
 import { connect } from 'react-redux'
 import './Card.css'
 
